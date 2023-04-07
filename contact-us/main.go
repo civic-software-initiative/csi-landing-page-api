7e9193f61dd19589bdd31882000c1031ddb9d0c7
Nikhil Vengal <ngvengal@gmail.com>
2023-04-07 10:34:40 -0700

    Impl test version of contact-us function

--- contact-us/main.go
+++ contact-us/main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"bytes"
 	"context"
-	"encoding/json"
+	"fmt"
+	"net/smtp"
+	"os"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
@@ -17,23 +19,29 @@ type Response events.APIGatewayProxyResponse
 
 // Handler is our lambda handler invoked by the `lambda.Start` function call
 func Handler(ctx context.Context) (Response, error) {
-	var buf bytes.Buffer
+	from := os.Getenv("SENDER")
+	password := os.Getenv("APP_PASSWORD")
+	to := []string{os.Getenv("RECEIVER")}
 
-	body, err := json.Marshal(map[string]interface{}{
-		"message": "Go Serverless v1.0! Your function executed successfully!",
-	})
+	var message bytes.Buffer
+	message.WriteString("Subject: Test Email\r\n")
+	message.WriteString("Hello World\r\n")
+
+	auth := smtp.PlainAuth("", from, password, "smtp.gmail.com")
+
+	err := smtp.SendMail("smtp.gmail.com:587", auth, from, to, message.Bytes())
 	if err != nil {
-		return Response{StatusCode: 404}, err
+		fmt.Println(err)
+		return Response{StatusCode: 400}, err
 	}
-	json.HTMLEscape(&buf, body)
 
 	resp := Response{
 		StatusCode:      200,
 		IsBase64Encoded: false,
-		Body:            buf.String(),
+		Body:            "",
 		Headers: map[string]string{
 			"Content-Type":           "application/json",
-			"X-MyCompany-Func-Reply": "hello-handler",
+			"X-MyCompany-Func-Reply": "contact-us-handler",
 		},
 	}
 
--- go.mod
+++ go.mod
@@ -2,5 +2,4 @@ module github.com/civic-software-initiative/csi-landing-page-api
 
 go 1.20
 
-require github.com/joho/godotenv v1.4.0
 require github.com/aws/aws-lambda-go v1.39.1
