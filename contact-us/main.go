8405e3dad1c933ad03c52650703449ddb9e4342f
Nikhil Vengal <ngvengal@gmail.com>
2023-04-07 13:56:25 -0700

    Switch to aws ses

--- contact-us/main.go
+++ contact-us/main.go
@@ -9,6 +9,10 @@ import (
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/ses"
 )
 
 // Response is of type APIGatewayProxyResponse since we're leveraging the
@@ -16,32 +20,93 @@ import (
 //
 // https://serverless.com/framework/docs/providers/aws/events/apigateway/#lambda-proxy-integration
 type Response events.APIGatewayProxyResponse
+type Request events.APIGatewayProxyRequest
 
 // Handler is our lambda handler invoked by the `lambda.Start` function call
-func Handler(ctx context.Context) (Response, error) {
-	from := os.Getenv("SENDER")
-	password := os.Getenv("APP_PASSWORD")
-	to := []string{os.Getenv("RECEIVER")}
+func Handler(ctx context.Context, request Request) (Response, error) {
+	sender := os.Getenv("SENDER")
+	recipient := []string{os.Getenv("RECIPIENT")}
+	region := os.Getenv("REGION")
 
-	var message bytes.Buffer
-	message.WriteString("Subject: Test Email\r\n")
-	message.WriteString("Hello World\r\n")
+	sess, err := session.NewSession(&aws.Config{
+		Region: aws.String(region)},
+	)
 
-	auth := smtp.PlainAuth("", from, password, "smtp.gmail.com")
-
-	err := smtp.SendMail("smtp.gmail.com:587", auth, from, to, message.Bytes())
 	if err != nil {
 		fmt.Println(err)
-		return Response{StatusCode: 400}, err
+		return Response{StatusCode: 500}, err
+	}
+
+	subject := "SES Test Email"
+	charset := "UTF-8"
+
+	// The HTML body for the email.
+	htmlBody := "<h1>Amazon SES Test Email (AWS SDK for Go)</h1><p>This email was sent with " +
+		"<a href='https://aws.amazon.com/ses/'>Amazon SES</a> using the " +
+		"<a href='https://aws.amazon.com/sdk-for-go/'>AWS SDK for Go</a>.</p>"
+
+	//The email body for recipients with non-HTML email clients.
+	textBody := "This email was sent with Amazon SES using the AWS SDK for Go."
+
+	svc := ses.New(sess)
+
+	input := &ses.SendEmailInput{
+		Destination: &ses.Destination{
+			CcAddresses: []*string{},
+			ToAddresses: []*string{
+				aws.String(recipient),
+			},
+		},
+		Message: &ses.Message{
+			Body: &ses.Body{
+				Html: &ses.Content{
+					Charset: aws.String(charset),
+					Data:    aws.String(htmlBody),
+				},
+				Text: &ses.Content{
+					Charset: aws.String(charset),
+					Data:    aws.String(textBody),
+				},
+			},
+			Subject: &ses.Content{
+				Charset: aws.String(charset),
+				Data:    aws.String(subject),
+			},
+		},
+		Source: aws.String(sender),
+	}
+
+	// Attempt to send the email.
+	result, err := svc.SendEmail(input)
+
+	// Display error messages if they occur.
+	if err != nil {
+		if aerr, ok := err.(awserr.Error); ok {
+			switch aerr.Code() {
+			case ses.ErrCodeMessageRejected:
+				fmt.Println(ses.ErrCodeMessageRejected, aerr.Error())
+			case ses.ErrCodeMailFromDomainNotVerifiedException:
+				fmt.Println(ses.ErrCodeMailFromDomainNotVerifiedException, aerr.Error())
+			case ses.ErrCodeConfigurationSetDoesNotExistException:
+				fmt.Println(ses.ErrCodeConfigurationSetDoesNotExistException, aerr.Error())
+			default:
+				fmt.Println(aerr.Error())
+			}
+		} else {
+			// Print the error, cast err to awserr.Error to get the Code and
+			// Message from an error.
+			fmt.Println(err.Error())
+		}
+
+		return Response{StatusCode: 500}, err
 	}
 
 	resp := Response{
 		StatusCode:      200,
 		IsBase64Encoded: false,
-		Body:            "",
+		Body:            result,
 		Headers: map[string]string{
-			"Content-Type":           "application/json",
-			"X-MyCompany-Func-Reply": "contact-us-handler",
+			"Content-Type": "application/json",
 		},
 	}
 
--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/civic-software-initiative/csi-landing-page-api
 
 go 1.20
 
+require github.com/aws/aws-sdk-go v1.44.239
+
 require github.com/aws/aws-lambda-go v1.39.1
+
+require github.com/jmespath/go-jmespath v0.4.0 // indirect
